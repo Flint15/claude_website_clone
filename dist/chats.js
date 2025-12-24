a2bfa7543173c8408bbf600ed237e784b1597691
Flint15 <chesspace341@gmail.com>
2025-12-24 13:08:54 +0100

    Fixed chat settings style

--- dist/chats.js
+++ dist/chats.js
@@ -34,13 +34,13 @@ function renderChat(chatId) {
     console.log(chats, currentChatId);
     chats.toReversed().forEach(chat => {
         html += `
-      <div class="chat-container">
+      <div class="chat-container ${chat.chatId === currentChatId ? 'current-chat' : ''}">
         <a
-          class="chat ${chat.chatId === currentChatId ? 'current-chat' : ''}"
+          class="chat"
           href="./new.html?chat-id=${chat.chatId}">
           ${chat.name}
         </a>
-        <button class="chat-settings can-focus">
+        <button class="chat-settings">
           <svg xmlns="http://www.w3.org/2000/svg" height="22px" viewBox="0 -960 960 960" width="22px" fill="#F3F3F3"><path d="M240-400q-33 0-56.5-23.5T160-480q0-33 23.5-56.5T240-560q33 0 56.5 23.5T320-480q0 33-23.5 56.5T240-400Zm240 0q-33 0-56.5-23.5T400-480q0-33 23.5-56.5T480-560q33 0 56.5 23.5T560-480q0 33-23.5 56.5T480-400Zm240 0q-33 0-56.5-23.5T640-480q0-33 23.5-56.5T720-560q33 0 56.5 23.5T800-480q0 33-23.5 56.5T720-400Z"/></svg>
         </button>
       </div>
